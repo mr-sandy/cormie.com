f4882d67321a72710479324788ef96ec22611241
Sandy Cormie <sandycormie@hotmail.com>
2023-12-27 10:49:21 +0000

    wip

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react';
+import * as images from "./images";
+import useImagePreloader from './useImagePreloader';
 
+// https://stackoverflow.com/questions/42615556/how-to-preload-images-in-react-js
+
+const horizontalImages = [images.left6, images.left5, images.left4, images.left3, images.left2, images.left1, images.centre1, images.centre2, images.right0, images.right1, images.right2, images.right3, images.right4, images.right5, images.right6];
+
+function preloadImage(src: string) {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = function () {
+      resolve(img)
+    }
+    img.onerror = img.onabort = function () {
+      reject(src)
+    }
+    img.src = `/img/${src}`
+  })
+}
 function App() {
+  const [index, setIndex] = useState<number>(Math.trunc(horizontalImages.length / 2));
+  const [assetsLoaded, setAssetsLoaded] = useState<boolean>(false)
+
+  const { imagesPreloaded } = useImagePreloader(horizontalImages)
+
+  // useEffect(() => {
+  //   let isCancelled = false
+
+  //   async function effect() {
+  //     if (isCancelled) {
+  //       return
+  //     }
+
+  //     const imagesPromiseList: Promise<any>[] = []
+  //     for (const i of horizontalImages) {
+  //       imagesPromiseList.push(preloadImage(i))
+  //     }
+
+  //     await Promise.all(imagesPromiseList)
+
+  //     if (isCancelled) {
+  //       return
+  //     }
+
+  //     setAssetsLoaded(true)
+  //   }
+
+  //   effect()
+
+  //   return () => {
+  //     isCancelled = true
+  //   }
+  // }, [])
+
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => setIndex(i => (i + 1)), 1000);
+  //   return () => clearInterval(timer);
+  // }, []);
+
+
+  // handle what happens on key press
+  const handleKeyPress = (e: KeyboardEvent) => {
+    switch (e.key) {
+      case "ArrowRight":
+        setIndex(i => (i + 1) % horizontalImages.length);
+        break;
+      case "ArrowLeft":
+        setIndex(i => ((i === 0) ? horizontalImages.length -1 : i - 1));
+        break;
+      default:
+        console.log(e.key)
+    }
+  };
+
+  // useEffect(() => {
+  //   horizontalImages.forEach(img => new Image().src = img)
+  // })
+
+  useEffect(() => {
+    // attach the event listener
+    document.addEventListener('keydown', handleKeyPress);
+
+    // remove the event listener
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [handleKeyPress]);
+
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${horizontalImages[index]})`;
+  }, [index]);
+
+  // if (!imagesPreloaded) {
+  //   return <p>Preloading Assets</p>
+  // }
+
+  // return <p>Assets Finished Preloading</p>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>{index}
     </div>
   );
 }
--- manual steps.md
+++ manual steps.md
@@ -4,5 +4,33 @@ https://medium.com/geekculture/creating-and-deploying-express-app-to-lambda-8114
 aws s3api create-bucket --bucket cormie-com-deployment-bucket --region eu-north-1 --create-bucket-configuration LocationConstraint=eu-north-1
 
 ## Create ECR repo
-aws ecr create-repository --repository-name cormie-com
+aws ecr create-repository --repository-name cormie-com  
 
+
+
+aws ecr create-repository --repository-name not-yet-cormie-com
+
+
+
+DOCKER INSTALL
+https://stackoverflow.com/questions/40523307/brew-install-docker-does-not-include-docker-engine
+Please try running
+
+brew install docker
+This will install the Docker engine, which will require Docker-Machine (+ VirtualBox) to run on the Mac.
+
+If you want to install the newer Docker for Mac, which does not require virtualbox, you can install that through Homebrew's Cask:
+
+brew install --cask docker 
+open /Applications/Docker.app
+
+
+
+aws s3api create-bucket --bucket not-yet-cormie-com-deployment-bucket --region eu-north-1 --create-bucket-configuration LocationConstraint=eu-north-1
+
+
+export AWS_ACCESS_KEY_ID="ASIA4B2QJ27TF2HMAL44"
+export AWS_SECRET_ACCESS_KEY="JDF5tOQH1R0SBG1FiuRhtA/8LTeOsTVGldvkj58C"
+export AWS_SESSION_TOKEN="IQoJb3JpZ2luX2VjEDgaCmV1LW5vcnRoLTEiSDBGAiEA6b+LyEi7OFb8UYsDp/yZhLPJ4ALU/eZ+WxHKpDnt/3kCIQDdpc+iudFZiJyqDISMvCTfsBgjuVqQ6A5v+jZy7y1KTSqGAwiS//////////8BEAMaDDgyODU2MDIzNDQ3MCIM01zLTflXvosGXcgVKtoCfk87TjGvcyL5r1YwjdD/Mx9jidImY5b6eM9881/j6mZuE01LudzhJnVS4/o+oxQyouIGcRtFOf7gIFKyNFmBDqX1g6lmRS/e3f3JS/fCAr4fLa2ohjnXbR4yZRB0/2mOpB2DR71aGgRNH9GkJ5fLNtvWtE4LZeMHPfSBmjopANVGpWNtlv5wti7SLiLyGVQ4Dp5Y5TmiK/z+eYfaSY32fVpazneFKer8j+FiwVIXgkqMQMgxYjEp1lycgZkd+W4AHRCdHvNzKKPj5bUtqY41qxmaykKDJBLNf4YKSsG6sVnCGL63WVKdS1GnpvsXSyklYSVLCUvwdc24+GHA+fQF/G2jxXfAJOM9HAb+x/5T9S+R1pdg2CsHZ1lNDLtJWDOnO7jn2XlsPoz01EKslxqqse6lbv5sA3gqRH5iCzBJoCEKZyxr7K9CXYPBwPGONqqlRcEvImDHQchTrjCR6Y2rBjqmAadF4iOdES214HiGaiW3Idlk6AOajOmpwROA1T0L7sVHLBDsi36XIKgIaYn0ulT4Aa7EL71Bp9bjTEMYNhCHJQwnAFKCr28xVLcm6tT+qKqWROleIDlGbMjKnJxbpYdOm+WN6CqwqanYB5qqwa/kNCenqgUZMvIxvR7ApFE/LwuyLcOFo1HKyhaNeYOCdWlG9xkC94tkn3UsSkvHq2e/SN30o0NIK3Y="
+
+aws configure sso
